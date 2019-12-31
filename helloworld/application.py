8dd51c354fc578a2ee55580c8edaa07a76f9590c
Aleksandar Glisovic <a.glisovic@logispin.com>
2019-12-31 13:47:12 +0100

    Add templates

--- helloworld/application.py
+++ helloworld/application.py
@@ -1,17 +1,31 @@
 #!flask/bin/python
 import json
-from flask import Flask, Response
+import functools
+from flask import Flask, Response, session, redirect, render_template
 from helloworld.flaskrun import flaskrun
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if 'user_id' not in session or session['user_id'] is None:
+            return redirect('/login')
+        return view(**kwargs)
+    return wrapped_view
+
 
 application = Flask(__name__)
 
-@application.route('/', methods=['GET'])
-def get():
-    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
 
-@application.route('/', methods=['POST'])
-def post():
-    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+@application.route('/', methods=['GET'])
+def index():
+    return Response(render_template('index.html'), mimetype='text/html', status=200)
+
+
+@application.route('/auth/register', methods=['GET', 'POST'])
+def register():
+    return Response(render_template('auth/register.html'), mimetype='text/html', status=200)
+
 
 if __name__ == '__main__':
     flaskrun(application)
