9d2d5c86caf4a1e0090f593a25ad32a2ce81cfaf
Aleksandar Glisovic <a.glisovic@logispin.com>
2019-12-31 14:00:21 +0100

    Fix urls

--- helloworld/application.py
+++ helloworld/application.py
@@ -1,7 +1,7 @@
 #!flask/bin/python
 import json
 import functools
-from flask import Flask, Response, session, redirect, render_template
+from flask import Flask, Response, session, redirect, render_template, url_for
 from helloworld.flaskrun import flaskrun
 
 
@@ -9,7 +9,7 @@ def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if 'user_id' not in session or session['user_id'] is None:
-            return redirect('/login')
+            return redirect(url_for('login'))
         return view(**kwargs)
     return wrapped_view
 
@@ -26,6 +26,21 @@ def index():
 def register():
     return Response(render_template('auth/register.html'), mimetype='text/html', status=200)
 
+@application.route('/auth/login', methods=['GET', 'POST'])
+def login():
+    return Response(render_template('auth/login.html'), mimetype='text/html', status=200)
+
+@application.route('/auth/logout', methods=['GET', 'POST'])
+def logout():
+    return redirect(url_for('login'))
+
+@application.route('/auth/request_password', methods=['GET', 'POST'])
+def request_password():
+    return Response(render_template('auth/request_password.html'), mimetype='text/html', status=200)
+
+@application.route('/auth/reset_rassword', methods=['GET', 'POST'])
+def reset_rassword():
+    return Response(render_template('auth/reset_rassword.html'), mimetype='text/html', status=200)
 
 if __name__ == '__main__':
     flaskrun(application)
